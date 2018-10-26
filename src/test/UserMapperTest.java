7ce3272f3e36d528bf9d9c5e36b8ffd6c72b55db
zyb <794357265@qq.com>
2018-10-26 10:43:27 +0800

    1

--- src/test/UserMapperTest.java
+++ src/test/UserMapperTest.java
@@ -1,4 +1,4 @@
-package dao;
+package test;
 
 import java.io.InputStream;
 import java.util.List;
@@ -11,6 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import domain.User;
+import mapper.UserMapper;
 
 public class UserMapperTest {
 
